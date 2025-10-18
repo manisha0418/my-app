9299d7017610189c132497b3dc4e3091e1cc607e
MinMan <manishavutturi04@gmail.com>
2025-10-18 14:49:23 -0500

    test

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Spring Boot running on port 8080!");
+        return "index";
+    }
+}
+
